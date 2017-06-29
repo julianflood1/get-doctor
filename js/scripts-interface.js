4ffe19cd549d5eeaa8ca1321bfa82fe81573e56c
Guest User <Guest@Epicodus-4F.local>
2017-06-29 13:52:20 -0700

    complete stolen bike lister by area

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -1,6 +1,19 @@
-var ExampleModule = require('./../js/scripts.js').exampleModule;
+var Bike = require('./../js/scripts.js').bikeModule;
+var bikeObject = new Bike();
 
+var displayBike = function(allBikes) {
+  for(var i = 0; i < allBikes.length; i++) {
+  $('.showBikes').append('<h3>Bike type: ' +  allBikes[i].title + '</h3><li>Bike serial: ' + allBikes[i].serial + '</li><li>Date stolen: ' + allBikes[i].date_stolen + '</li>');
+}
+};
 
 $(document).ready(function() {
+  $('#stolen-bikes').click(function(){
+    var location = $('#location').val();
+    // $('#location').val('');
+    var proximity = $('#proximity').val();
+    // $('#proximity').val('');
+    bikeObject.getBike(location, proximity, displayBike);
 
+  });
 });
--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,23 @@
-//business logic stuff for a particular module
-var ExampleModule = function(args) {
-  this.args = args; //to be replaced with constructor arguments
+
+Bike = function(title, serial, date_stolen){
+  this.title = title;
+  this.serial = serial;
+  this.date_stolen = date_stolen;
+}
+
+Bike.prototype.getBike = function(location, proximity, displayBike) {
+  $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=50&location=' + location + '&distance=' + proximity + '&stolenness=proximity').then(function(response) {
+    var allBikes = [];
+    for(var i = 0; i < response.bikes.length; i++){
+      var newBike = new Bike(response.bikes[i].title, response.bikes[i].serial, response.bikes[i].date_stolen);
+      allBikes.push(newBike);
+    }
+    console.log(allBikes[0].title);
+    displayBike(allBikes);
+  });
+  .fail(function(error) {
+    $('.showBikes').text(error.responseJSON.message);
+  });
 };
 
-ExampleModule.prototype.examplePrototype = function() {
-  return 'this is an example prototype method';
-};
-
-exports.exampleModule = ExampleModule;
+exports.bikeModule = Bike;
