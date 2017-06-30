494bc721c7516ef406b166805894838f0208ecc6
Julian Flood <Julian@Julians-MacBook-Pro.local>
2017-06-30 15:54:36 -0700

    finalize objectives, prepare readme

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -2,7 +2,7 @@ var Doctor = require('./../js/scripts.js').doctorListModule;
 
 var displayDoctors = function(allDoctors) {
   for(i = 0; i < allDoctors.length; i++ ) {
-  $('.showDoctors').append('<div class ="panel"><h2>' + allDoctors[i].first_name + " " + allDoctors[i].last_name + '</h2><img id="doctor-img" src =' + allDoctors[i].image_url + '><textarea>' + allDoctors[i].bio + '</textarea></div>')
+  $('.showDoctors').append('<div class ="panel"><h2>' + allDoctors[i].first_name + " " + allDoctors[i].last_name + '</h2><img id="doctor-img" src =' + allDoctors[i].image_url + '><p>' + 'Specialty: ' + allDoctors[i].name + '</p><textarea>' + allDoctors[i].bio + '</textarea></div>')
   }
 };
 
@@ -18,7 +18,6 @@ $(document).ready(function() {
     }
     $('#symptom').val('');
 
-
     doctorObject.getDoctor(symptom, displayDoctors);
-  });
+    });
 });
--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,24 @@
 var apiKey = require('./../.env').apiKey;
 
-function Doctor(first_name, last_name, bio, image_url) {
+function Doctor(first_name, last_name, bio, image_url, name) {
   this.first_name = first_name;
   this.last_name = last_name;
   this.bio = bio;
   this.image_url = image_url;
+  this.name = name;
 }
 
 Doctor.prototype.getDoctor = function(symptom, displayDoctors) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + symptom + '&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=20&user_key=' + apiKey)
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ symptom + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
   .then(function(response) {
-    console.log(response.data[0].profile.first_name);
-
     var allDoctors = [];
     var doctorOutput = response.data;
     for(i = 0; i < doctorOutput.length; i++) {
-      var newDoctor = new Doctor(response.data[i].profile.first_name, response.data[i].profile.last_name, response.data[i].profile.bio, response.data[i].profile.image_url);
+      var newDoctor = new Doctor(response.data[i].profile.first_name, response.data[i].profile.last_name, response.data[i].profile.bio, response.data[i].profile.image_url, response.data[i].specialties[0].name);
       allDoctors.push(newDoctor);
     }
-    console.log(allDoctors);
-
     displayDoctors(allDoctors);
   })
-
-
 
   .fail(function(error){
     $('.showDoctors').text(error.responseJSON.message);
