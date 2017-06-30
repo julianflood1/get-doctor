0d53c59530c330539d0db7b40b0d9e1d758faeaf
Julian Flood <Julian@Julians-MacBook-Pro.local>
2017-06-30 13:55:12 -0700

    add bulk of styling, work on adding more features

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -2,7 +2,7 @@ var Doctor = require('./../js/scripts.js').doctorListModule;
 
 var displayDoctors = function(allDoctors) {
   for(i = 0; i < allDoctors.length; i++ ) {
-  $('.showDoctors').append('<div class ="panel"><h2>' + allDoctors[i].first_name + " " + allDoctors[i].last_name + '</h2><img id="doctor-img" src =' + allDoctors[i].image_url + '><textarea>' + allDoctors[i].bio + '</textarea></div>' );
+  $('.showDoctors').append('<div class ="panel"><h2>' + allDoctors[i].first_name + " " + allDoctors[i].last_name + '</h2><img id="doctor-img" src =' + allDoctors[i].image_url + '><textarea>' + allDoctors[i].bio + '</textarea></div>')
   }
 };
 
@@ -11,7 +11,13 @@ $(document).ready(function() {
   $('#symptom-search').click(function(){
     $('.showDoctors').empty();
     var symptom = $('#symptom').val();
+    $('#output-text').text('You seem to be experiencing ' + symptom + '. Below is a list of doctors that can help you with your ailment.');
+    if ($('#symptom').val() === '') {
+      alert('Please Enter a Symptom');
+      return;
+    }
     $('#symptom').val('');
+
 
     doctorObject.getDoctor(symptom, displayDoctors);
   });
