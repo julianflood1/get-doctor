fe15624e5645f0f5f293f847487a8c6df67aa324
Julian Flood <Julian@Julians-MacBook-Pro.local>
2017-06-30 10:20:39 -0700

    create basic functionality for displaying doctor first name based on user input, begin looping objects

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -1,15 +1,14 @@
-var ExampleModule = require('./../js/scripts.js').exampleModule;
+var Doctor = require('./../js/scripts.js').doctorListModule;
 
-var apiKey = "54105fb8349a0ff6c239a7e122d74e7f";
-
+var displayDoctors = function(symptom, doctorList) {
+  $('.showDoctors').text("If you are experiencing " + symptom + " , " + doctorList + " can help you.");
+};
 $(document).ready(function() {
+  var doctorObject = new Doctor();
   $('#symptom-search').click(function(){
     var symptom = $('#symptom').val();
     $('#symptom').val('');
-    $('.showIllness').text("You might have " + symptom + ".");
-    $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + symptom + '&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=20&user_key=' + apiKey, function(response) {
-      console.log(response.data[0].specialties);
-    });
 
+    doctorObject.getDoctor(symptom, displayDoctors);
   });
 });
--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,24 @@
-Example = function(){
+var apiKey = require('./../.env').apiKey;
 
+function Doctor() {
 }
 
-Example.prototype.Example = function() {
+Doctor.prototype.getDoctor = function(symptom, displayDoctors) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + symptom + '&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=20&user_key=' + apiKey)
+  .then(function(response) {
+    console.log(response.data);
 
+
+    // for(i = 0; i < response.data.length; i++) {
+    displayDoctors(symptom, response.data[0].profile.first_name);
+  })
+// };
+
+
+
+  .fail(function(error){
+    $('.showDoctors').text(error.responseJSON.message);
+  });
 };
 
-exports.exampleModule = Example;
+exports.doctorListModule = Doctor;
